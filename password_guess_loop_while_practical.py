95e3e7256d43900ec40d79e3a22db58349e8d781
Ykamesh1 <jokamesh@gmail.com>
2026-03-11 16:09:12 +0200

    Add files via upload

---
+++ password_guess_loop_while_practical.py
@@ -0,0 +1,37 @@
+# ----------------------------
+# -- loop => while training --
+# -- simple_password_guesss --
+# ----------------------------
+
+tries = 4 
+
+mainpassword = "kamesh123"
+
+inputpassword = input("type your password: ")
+
+while inputpassword != mainpassword :
+    
+    tries -= 1 # tries = tries -1 
+    
+    print(f"wrong password, { "last" if tries == 0 else tries } chances left")
+    
+    inputpassword = input("type your password: ")
+    
+    if tries == 0 :
+        
+        print("all tries is finished")
+        
+        break      
+    
+        print("will not print")    
+    
+else:
+    print("password is correct")    
+    
+    
+    
+    
+    
+    
+    
+    
